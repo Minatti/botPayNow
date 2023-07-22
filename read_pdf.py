5c75bf561aca6cb7528e55c919a5f29b9b33b327
Luan Carminatti <luancarminatt@gmail.com>
2023-07-22 20:58:05 -0300

    module read pdf for extract data

--- read_pdf.py
+++ read_pdf.py
@@ -1,17 +1,41 @@
 from PyPDF2 import PdfReader
 
-path = "C:/users/workcarminatti/Downloads/arquivo.pdf"
-print(path)
-
-reader = PdfReader(path)
-page = reader.pages[0]
-text = page.extract_text()
-rows = text.split("\n")
 
 
-for i in rows:
-    k = i.split(",")
-    print(k)
- 
+def read_my_pdf():
 
-    
+    path = "C:/users/workcarminatti/Downloads/arquivo.pdf"
+
+    reader = PdfReader(path)
+    page = reader.pages[0]
+    text = page.extract_text()
+    lista_itens = text.split("\n")
+
+
+    # Descobrindo posição do valor (cod barra)
+    for idx, values in enumerate(lista_itens):
+        idx, values
+
+    values = []
+
+    values.append(lista_itens[57])
+    values.append(lista_itens[59])
+
+    return values
+
+
+def get_date():
+    dt = read_my_pdf()
+    split_date = dt[1].split(" ")
+
+
+
+    return split_date[0]
+
+
+codigo = read_my_pdf()
+vencimento = get_date()
+
+print("DETALHES...")
+print("Codigo:", codigo[0])
+print("Vencimento", vencimento)
